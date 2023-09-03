82c418d47043533724466929b95fe81200e6551e
Elizabeth <tjeudy.e@gmail.com>
2023-09-03 12:42:42 -0400

    import/export menu items data

---
+++ data.js
@@ -0,0 +1,64 @@
+export const menuItems = [
+	{
+		item: "Pizza",
+		itemIcon: "pizza.png",
+		toppings: ["pepperoni, mushroom, cheese"],
+		price: $14,
+	},
+	{
+		item: "Burger",
+		itemIcon: "burger.png",
+		toppings: ["bacon, cheese, tomato, pickles, lettuce"],
+		price: $12,
+	},
+	{
+		item: "Taco",
+		itemIcon: "taco.png",
+		toppings: ["beef, brown rice, black beans, cheese, sour cream"],
+		price: $2,
+	},
+	{
+		item: "Hotdog",
+		itemIcon: "hotdog.png",
+		toppings: ["ketchup, mustard, relish"],
+		price: $4,
+	},
+	{
+		item: "Corndog",
+		itemIcon: "corndog.png",
+		toppings: ["ketchup, mustard"],
+		price: $3,
+	},
+	{
+		item: "Chicken Wrap",
+		itemIcon: "kebab.png",
+		toppings: ["chicken, bacon, lettuce, cheese, tomato"],
+		price: $10,
+	},
+	{
+		item: "Fries",
+		itemIcon: "french-fries.png",
+		toppings: ["ketchup, bbq sauce, honey mustard"],
+		price: $2,
+	},
+	{
+		item: "Beer",
+		itemIcon: "beer.png",
+		toppings: [
+			"Budweiser, Heineken, Miller Lite, Stella Artois, Michelob Ultra, Corona",
+		],
+		price: $5,
+	},
+	{
+		item: "Soda",
+		itemIcon: "soda.png",
+		toppings: ["Dr.Pepper, Coke, Pepsi, Sprite, Mountain Dew"],
+		price: $2,
+	},
+	{
+		item: "Ice Cream",
+		itemIcon: "ice-cream.png",
+		toppings: ["vanilla, chocolate, pecan"],
+		price: $4,
+	},
+];
