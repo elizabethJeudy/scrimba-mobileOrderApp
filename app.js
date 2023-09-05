4332d86fe161728609288f57aaa7048f9663905e
Elizabeth <tjeudy.e@gmail.com>
2023-09-04 22:20:07 -0400

    added/styled footer nav icons

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-import { menuItems } from "/data.js";
+import { menuArray } from "/data.js";
 
-const menuItemHtml = menuItems
+const menuArrHtml = menuArray
 	.map(function (item) {
 		return `
   <section class="card">
         <div class="card-start">
-            <img src="/assets/${item.itemIcon}">
+            <img src="/assets/${item.icon}">
         </div>
             <div class="card-mid">
-                <h3 class="card-itemName">${item.itemName} $${item.price}</h3>
+                <h3 class="card-name">${item.name} $${item.price}</h3>
                 <p class="card-toppings">${item.toppings}</p>
                 
             </div>
         <div class="card-end">
             <p class="card-menu">
-               <img src="/assets/add-white.png" class="img-add-white"/>
+               <img src="/assets/add-black.png" class="img-add-black"/>
             </p>
         </div>
     </section>
   `;
 	})
 	.join("");
-document.getElementById("container").innerHTML = menuItemHtml;
+document.getElementById("container").innerHTML = menuArrHtml;
